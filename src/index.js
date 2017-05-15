ca7ed5faf991ab3f544d3d3b324f3456dfb0d010
Bryan Fillmer <bryan.fillmer@gmail.com>
2017-05-15 16:42:22 -0600

    Basic cleanup.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Provider} from 'react-redux'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+import 'tachyons'
+
+import {App} from 'view/App'
+
+import {store} from 'state/store'
+
+ReactDOM.render((
+  <Provider store={store}>
+    <App />
+  </Provider>
+), document.getElementById('root'))
